8fde677dab4811535406531824fc5d4c5c3d8f80
poiop <poiopoiop@sina.com>
2017-03-23 09:26:46 +0800

    modify paths

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -I ../libevent/output/include/ -c event.cpp -o event.o
-g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c log.cpp -o log.o
-g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c test.cpp -o test.o
-g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c file.cpp -o file.o 
-g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c main.cpp -o main.o 
+g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -I ../libevent/output/include/ -c ./src/event.cpp -o ./src/event.o
+g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c ./src/log.cpp -o ./src/log.o
+g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c ./src/main.cpp -o ./src/main.o
+g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c ./src/file.cpp -o ./src/file.o 
+g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -c ./src/main.cpp -o ./src/main.o 
 #g++ -g -Wall -I ../bd_libs/public/configure/output/include/ -I ../bd_libs/lib2-64/bsl/include/ -I ../bd_libs/lib2-64/ullib/include/ -I ../libevent/output/include/ -L ../bd_libs/public/configure/output/lib/ -L ../bd_libs/lib2-64/bsl/lib/ -L ../bd_libs/lib2-64/ullib/lib/ -L ../libevent/output/lib/ event.o log.o test.o -o test -lpthread -levent
 
 g++ -g -Wall \
@@ -11,10 +11,10 @@ g++ -g -Wall \
     -I ../bd_libs/lib2-64/bsl/include/ \
     -I ../bd_libs/lib2-64/ullib/include/ \
     -I ../libevent/output/include/ \
-    event.o \
-    log.o\
-    file.o\
-    main.o\
+    ./src/event.o \
+    ./src/log.o\
+    ./src/file.o\
+    ./src/main.o\
     -Xlinker "-(" \
     ../libevent/output/lib/libevent.a \
     ../bd_libs/lib2-64/bsl/lib/libbsl.a \
@@ -29,4 +29,4 @@ g++ -g -Wall \
     ../bd_libs/lib2-64/bsl/lib/libbsl_var_implement.a \
     ../bd_libs/lib2-64/bsl/lib/libbsl_var_utils.a \
     ../bd_libs/lib2-64/ullib/lib/libullib.a \
-    -lpthread -rdynamic -Xlinker "-)" -o test
+    -lpthread -rdynamic -Xlinker "-)" -o server
